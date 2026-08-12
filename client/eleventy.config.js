3794b9478f863514da607d129e4e99244a788ddb
Arun S V <svarunid>
2026-08-12 12:15:44 +0530

    feat: re-introduce `shuffle` filter in 11ty configuration

--- client/eleventy.config.js
+++ client/eleventy.config.js
@@ -14,6 +14,15 @@ export default function (eleventyConfig) {
     },
   });
 
+  eleventyConfig.addFilter("shuffle", (array) => {
+    const shuffled = [...array];
+    for (let i = shuffled.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+    }
+    return shuffled;
+  });
+
   return {
     dir: {
       input: "src",
